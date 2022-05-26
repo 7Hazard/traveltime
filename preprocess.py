e2a6746724012f25abd2118e2e46aed2ad71f0c3
Hazard <hazard9909@gmail.om>
2022-05-26 03:48:02 +0200

    Convert shapefiles coordinate system to world WGS84 coordinate system

--- preprocess.py
+++ preprocess.py
@@ -1,36 +1,28 @@
 import pyarrow.parquet as pq
 import pandas as pd
-import matplotlib.pyplot as plt
 
-trips = pq.read_table('data/trips/yellow_tripdata_2019-01.parquet')
-trips: pd.DataFrame = trips.to_pandas()
-trips = trips[["tpep_pickup_datetime", "tpep_dropoff_datetime", "trip_distance", "PULocationID", "DOLocationID"]]
+__trips = pq.read_table('data/trips/yellow_tripdata_2019-01.parquet')
+__trips: pd.DataFrame = __trips.to_pandas()
+__trips = __trips[["tpep_pickup_datetime", "tpep_dropoff_datetime", "trip_distance", "PULocationID", "DOLocationID"]]
 
 # remove all rows with trip distance of 0
-trips = trips[trips.trip_distance > 0.0]
+__trips = __trips[__trips.trip_distance > 0.0]
 
 # remove all rows with pickup time before 2019-01-01 00:00:00
-trips = trips[trips["tpep_pickup_datetime"] >= "2019-01-01 00:00:00"]
+__trips = __trips[__trips["tpep_pickup_datetime"] >= "2019-01-01 00:00:00"]
 
 # remove all rows with pickup time after dropoff time
-trips = trips[trips["tpep_pickup_datetime"] <= trips["tpep_dropoff_datetime"]]
+__trips = __trips[__trips["tpep_pickup_datetime"] <= __trips["tpep_dropoff_datetime"]]
 
 # remove all rows with dropoff time before 2019-01-01 00:00:00
-trips = trips[trips["tpep_dropoff_datetime"] >= "2019-01-01 00:00:00"]
+__trips = __trips[__trips["tpep_dropoff_datetime"] >= "2019-01-01 00:00:00"]
 
 # add column with trip time in seconds
-trips["trip_time"] = (pd.to_datetime(trips["tpep_dropoff_datetime"]) - pd.to_datetime(trips["tpep_pickup_datetime"])).dt.total_seconds()
+__trips["trip_time"] = (pd.to_datetime(__trips["tpep_dropoff_datetime"]) - pd.to_datetime(__trips["tpep_pickup_datetime"])).dt.total_seconds()
 
 # print 10 random rows
-print(trips.sample(10))
+# print(__trips.sample(10))
 
 # plot [tpep_pickup_datetime, trip_distance, PUlocationID, and DOLocationID] vs trip_time
 # trips.plot(x=["tpep_pickup_datetime", "trip_distance", "PULocationID", "DOLocationID"], y="trip_time", kind="scatter", figsize=(12, 6))
 # plt.scatter(trips["tpep_pickup_datetime"], trips["trip_distance"], trips["PULocationID"], trips["DOLocationID"], c=trips["trip_time"], cmap="tab20")
-
-# read all taxi zones from csv file
-taxi_zones = pd.read_csv("data/zones/taxi_zone_lookup.csv")
-
-# print 10 random rows from taxi_zones
-# print(taxi_zones.sample(10))
-
--- zones.py
+++ zones.py
@@ -1,36 +1,71 @@
 import shapefile as shp
 import matplotlib.pyplot as plt
-from shapely.geometry import shape, Point, Polygon
+from shapely.geometry import Point, Polygon
+import pandas as pd
 
-sf = shp.Reader("data/zones/taxi_zones.shp")
+# Dataset name:	taxi_zones
+# Format:	ESRI Shapefile (shp)
+# File size:	1737443 bytes
+# Layers count:	1
+# Dataset extent:
+# Min X: 913175.109008804
+# Min Y: 120121.88125434518
+# Max X: 1067382.508405164
+# Max Y: 272844.2940054685
+# Extent WGS 84 (lat/lon):
+# Min X: -74.25554696612534
+# Min Y: 40.496096946233486
+# Max X: -73.69921518371616
+# Max Y: 40.915175851595436
+# Coordinate system: +proj=lcc +lat_1=40.66666666666666 +lat_2=41.03333333333333 +lat_0=40.16666666666666 +lon_0=-74 +x_0=300000 +y_0=0 +datum=NAD83 +units=us-ft +no_defs
+# sf = shp.Reader("data/zones/taxi_zones.shp")
 
-plt.figure()
-for shape in sf.shapeRecords():
-    for i in range(len(shape.shape.parts)):
-        i_start = shape.shape.parts[i]
-        if i==len(shape.shape.parts)-1:
-            i_end = len(shape.shape.points)
-        else:
-            i_end = shape.shape.parts[i+1]
-        x = [i[0] for i in shape.shape.points[i_start:i_end]]
-        y = [i[1] for i in shape.shape.points[i_start:i_end]]
-        plt.plot(x,y)
-        # print shape metadata
-        # print(shape.record)
-        # print shape zone code
-        zoneCode = shape.record[0]
-        print(zoneCode)
-plt.show()
+__sf = shp.Reader("data/zones/taxi_zones.zip")
 
-def latlonToZone(lon, lat):
-    point = Point(lon, lat)
-    for sr in sf.shapeRecords():
+def plotZones():
+    plt.figure()
+    for shape in __sf.shapeRecords():
+        for i in range(len(shape.shape.parts)):
+            i_start = shape.shape.parts[i]
+            if i==len(shape.shape.parts)-1:
+                i_end = len(shape.shape.points)
+            else:
+                i_end = shape.shape.parts[i+1]
+            x = [i[0] for i in shape.shape.points[i_start:i_end]]
+            y = [i[1] for i in shape.shape.points[i_start:i_end]]
+            plt.plot(x,y)
+    plt.show()
+
+def latlonToZoneId(lon, lat):
+    x, y = lon, lat
+    point = Point(x, y)
+
+    # find the zone that contains the point
+    for sr in __sf.shapeRecords():
         sr: shp.ShapeRecord
         shape: shp.Shape = sr.shape
         polygon = Polygon(shape.points)
-        print(polygon)
-        if polygon.within(point):
+        if polygon.contains(point):
             return sr.record[0]
+
     return None
 
-print(latlonToZone(40.7128, -74.0060))
+# plotZones()
+# print(latlonToZoneId(-74.012130, 40.692293))
+
+# read all taxi zones from csv file
+__taxi_zones = pd.read_csv("data/zones/taxi_zone_lookup.csv")
+
+# print string Zone of zone LocationID 105
+# print(__taxi_zones[__taxi_zones.LocationID == 105]["Zone"].values[0])
+
+def zoneIdToName(id):
+    return __taxi_zones[__taxi_zones.LocationID == id]["Zone"].values[0]
+
+# print 10 random rows from taxi_zones
+# print(__taxi_zones.head(110))
+
+# print(zoneIdToName(latlonToZoneId(-74.012130, 40.692293)))
+
+
+
